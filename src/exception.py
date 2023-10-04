72d73927d5f52797fa8da24eb6213b80b1d41258
Srinubabuvelugu <srinubabu1911@gmail.com>
2023-10-04 21:13:00 +0530

    changes made

--- src/exception.py
+++ src/exception.py
@@ -14,8 +14,8 @@ class CustomException(Exception):
         super().__init__(error_msg)
         self.error_msg = error_msg_detail(error_msg,error_detail = error_detail)
 
-        def __str__(self):
-            return self.error_msg
+    def __str__(self):
+        return self.error_msg
 if __name__ == "__main__":
     logging.info("Logging has started")
 
